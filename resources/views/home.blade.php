414188faa73b411fcff4301de44217581571e5af
umeshmechx <umesh.mechx@gmail.com>
2018-08-17 18:06:39 +0530

    update

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,7 +14,7 @@
                         </div>
                     @endif
 
-                    You are logged in!
+                    Welcome {{ Auth::user()->name }}
                     <example></example>
                 </div>
             </div>
