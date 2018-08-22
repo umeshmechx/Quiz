1ec3bc0eb3c05282458414a8ea8102e3c889d4a6
umeshmechx <umesh.mechx@gmail.com>
2018-08-22 14:09:29 +0530

    login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -41,13 +41,7 @@
                         </div>
 
                         <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                                <div class="checkbox">
-                                    <label>
-                                        <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> Remember Me
-                                    </label>
-                                </div>
-                            </div>
+                          
                         </div>
 
                         <div class="form-group">
